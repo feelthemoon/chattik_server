8fb96f1467b4b89b2acdfee660a579e5bc83f1e8
Vladislav Vdovin <programmer28.vdovin@yandex.ru>
2022-06-23 01:45:09 +0300

    fix: some little fixes && added .git-hook

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,13 @@ async function bootstrap() {
   const PORT = process.env.APP_PORT || 8000;
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.enableCors({
+    origin: 'http://localhost:8000/',
+    credentials: true,
+    allowedHeaders: 'Origin, Content-Type, X-Auth-Token',
+    methods: 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+    optionsSuccessStatus: 200
+  });
   await app.listen(PORT, () =>
     Logger.verbose(`APP HAS BEEN STARTED AT PORT ${PORT}`),
   );
