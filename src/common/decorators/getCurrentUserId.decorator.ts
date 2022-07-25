5b20a1850399362f81a43cc58d96b317724913c5
Vladislav Vdovin <programmer28.vdovin@yandex.ru>
2022-07-25 23:12:18 +0300

    feat: added getting user info

--- src/common/decorators/getCurrentUserId.decorator.ts
+++ src/common/decorators/getCurrentUserId.decorator.ts
@@ -6,7 +6,15 @@ export const GetCurrentUserIdFromRefreshToken = createParamDecorator(
   (_: undefined, context: ExecutionContext): number => {
     const request: Request = context.switchToHttp().getRequest();
     const jwt = new JwtService();
-    return jwt.decode(request.cookies['Refresh']).sub;
+    return jwt.decode(request.cookies['Refresh'])?.sub;
+  },
+);
+
+export const GetCurrentUserIdFromAccessToken = createParamDecorator(
+  (_: undefined, context: ExecutionContext): number => {
+    const request: Request = context.switchToHttp().getRequest();
+    const jwt = new JwtService();
+    return jwt.decode(request.headers.authorization.split(' ')[1])?.sub;
   },
 );
 
@@ -15,7 +23,7 @@ export const GetCurrentUserIdFromRecoverToken = createParamDecorator(
     const request: Request = context.switchToHttp().getRequest();
     const jwt = new JwtService();
     if (request.query.token && typeof request.query.token === 'string') {
-      return jwt.decode(request.query.token).sub;
+      return jwt.decode(request.query.token)?.sub;
     }
   },
 );
