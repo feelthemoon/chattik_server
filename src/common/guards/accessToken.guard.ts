69f2514bac80fc1d9f067052da55b3c3d9a71f96
Vladislav Vdovin <programmer28.vdovin@yandex.ru>
2022-07-25 17:44:25 +0300

    fix: refactored refreshing access token

--- src/common/guards/accessToken.guard.ts
+++ src/common/guards/accessToken.guard.ts
@@ -2,14 +2,25 @@ import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/com
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { TokenExpiredError } from 'jsonwebtoken';
+import { AuthenticationService } from '../../modules/authentication/authentication.service';
+import { JwtService } from '@nestjs/jwt';
+import { Request, Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AtGuard extends AuthGuard('jwt') {
-  constructor(private reflector: Reflector) {
+  constructor(
+    private reflector: Reflector,
+    private readonly authService: AuthenticationService,
+    private readonly jwtService: JwtService,
+    private readonly config: ConfigService,
+  ) {
     super();
   }
 
-  canActivate(context: ExecutionContext) {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+    const response: Response = context.switchToHttp().getResponse();
     const isPublic = this.reflector.getAllAndOverride('isPublic', [
       context.getHandler(),
       context.getClass(),
@@ -17,15 +28,31 @@ export class AtGuard extends AuthGuard('jwt') {
 
     if (isPublic) return true;
 
-    return super.canActivate(context);
-  }
+    const accessToken = request.headers.authorization.split(' ')[1];
+    const refreshToken = request.cookies['Refresh'];
 
-  handleRequest(err, user, info) {
-    if (info instanceof TokenExpiredError) {
+    if (!accessToken || !refreshToken) {
       throw new UnauthorizedException({
-        message: [{ type: 'token_expired_error', text: 'Unauthorized' }],
+        message: [{ type: 'common_error', text: 'Unauthorized' }],
       });
     }
-    return user;
+    try {
+      this.jwtService.verify(accessToken, {
+        secret: this.config.get<string>('ACCESS_TOKEN_SECRET'),
+      });
+      return super.canActivate(context) as Promise<boolean>;
+    } catch (e) {
+      if (e instanceof TokenExpiredError) {
+        const newAccessToken = await this.authService.refreshToken(
+          this.jwtService.decode(accessToken).sub,
+          refreshToken,
+        );
+        request.headers.authorization = `Bearer ${newAccessToken}`;
+        response.setHeader('Access-Control-Expose-Headers', 'Authorization');
+        response.setHeader('Authorization', `Bearer ${newAccessToken}`);
+        return super.canActivate(context) as Promise<boolean>;
+      }
+      return false;
+    }
   }
 }
