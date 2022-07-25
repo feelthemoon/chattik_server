4fa1c755403e8a90d3223384a7796ceb7f00844d
Vladislav Vdovin <programmer28.vdovin@yandex.ru>
2022-07-25 17:44:25 +0300

    fix: refactored refreshing access token

--- src/modules/authentication/authentication.module.ts
+++ src/modules/authentication/authentication.module.ts
@@ -1,13 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { AuthenticationController } from './authentication.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
 import { AtStrategy, RtStrategy, VerifyTokenStrategy } from './strategies';
 
+@Global()
 @Module({
   imports: [JwtModule.register({}), UsersModule],
   providers: [AuthenticationService, AtStrategy, RtStrategy, VerifyTokenStrategy],
   controllers: [AuthenticationController],
+  exports: [AuthenticationService, JwtModule],
 })
 export class AuthenticationModule {}
